2d8ec9ca3d8f665ff01e237d339e0636e9f70cb2
a296644830 <13886634784@163.com>
2018-10-09 21:57:30 +0800

    '完成首页热卖及轮播'

--- taiy/src/main.js
+++ taiy/src/main.js
@@ -1,8 +1,41 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+
+import App from './App.vue';
+
+import VueRouter from 'vue-router';
+
+import './assets/statics/site/css/style.css';
+
+import index from './components/index.vue';
+
+import ElementUI from 'element-ui';
+
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+
+
+Vue.use(VueRouter)
+
+const routes=[
+  {
+    path:'/',
+    component:index   
+  },
+  {
+    path:'/index',
+    component:index  
+  },
+
+]
+
+const router = new VueRouter({
+  routes,
+  
+})
 
 Vue.config.productionTip = false
-
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount('#app')
